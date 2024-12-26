2c3910b0dcc1835676a292993f17a51de480839b
Ashish Jindal <jindal0290@gmail.com>
2024-12-26 14:31:19 +0530

    fix

--- src/pages/payment-and-refund-policy.tsx
+++ src/pages/payment-and-refund-policy.tsx
@@ -4,8 +4,10 @@ import {ParagraphMedium} from 'baseui/typography';
 import {ListItem, ListItemLabel} from 'baseui/list';
 import {Wrapper} from '../components/wrapper';
 import {Container} from '../components/container';
+import {useStyletron} from 'baseui';
 
 const PaymentAndRefundPolicy: React.FC = () => {
+  const [css, theme] = useStyletron();
   return (
     <Wrapper>
       <Container>
@@ -32,55 +34,104 @@ const PaymentAndRefundPolicy: React.FC = () => {
         </HeadingLevel>
         <ParagraphMedium>
           In the case of failed transactions, payments are generally refunded by
-          the bank within 5-7 working days. If it&apos;s believed the payment has
-          reached us erroneously, contact us with transaction details at{' '}
+          the bank within 5-7 working days. If it&apos;s believed the payment
+          has reached us erroneously, contact us with transaction details at{' '}
           <a href="mailto:support@otpsign.com">support@otpsign.com</a>.
         </ParagraphMedium>
         <ParagraphMedium>
           To assist us in resolving your transaction issue, please provide the
           following details:
         </ParagraphMedium>
-        <ul>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Transaction ID:</strong> The unique identifier given by
-              your bank or payment processor.
+        <ul className={css({margin: 0, padding: 0})}>
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="The unique identifier given by your bank or payment processor.">
+              <strong>Transaction ID:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Transaction Date and Time:</strong> To help us locate the
-              payment reference.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="To help us locate the payment reference.">
+              <strong>Transaction Date and Time:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Order Number:</strong> If applicable, to tie the payment
-              to your specific service request.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="If applicable, to tie the payment to your specific service request.">
+              <strong>Order Number:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Payment Method Details:</strong> Including UPI or debit
-              card usage with any available masked numbers.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="Including UPI or debit card usage with any available masked numbers.">
+              <strong>Payment Method Details:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>User Account Information:</strong> Such as the email
-              associated with your OTPSign.com account.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="Such as the email associated with your OTPSign.com account.">
+              <strong>User Account Information:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Screenshots or a Bank Statement:</strong> For verification
-              purposes, ensuring sensitive information is redacted.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="For verification purposes, ensuring sensitive information is redacted.">
+              <strong>Screenshots or a Bank Statement:</strong>
             </ListItemLabel>
           </ListItem>
-          <ListItem>
-            <ListItemLabel>
-              <strong>Narrative of the Issue:</strong> To describe any problems
-              experienced.
+          <ListItem
+            overrides={{
+              Root: {
+                style: {
+                  padding: `${theme.sizing.scale400} 0`,
+                },
+              },
+            }}
+          >
+            <ListItemLabel description="To describe any problems experienced.">
+              <strong>Narrative of the Issue:</strong>
             </ListItemLabel>
           </ListItem>
         </ul>
