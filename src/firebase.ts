e551d6de0f50aaa8b9a4253b872ad7c60fe53931
Ashish Jindal <jindal0290@gmail.com>
2024-12-26 12:08:41 +0530

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,12 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import CryptoJS from 'crypto-js';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyC4cVkfFPCIU_7_qtuRBKmK04k7ZLxKiB4',
-  authDomain: 'refined-window-438515-p1.firebaseapp.com',
-  databaseURL:
-    'https://refined-window-438515-p1-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'refined-window-438515-p1',
-  storageBucket: 'refined-window-438515-p1.firebasestorage.app',
-  messagingSenderId: '812821469928',
-  appId: '1:812821469928:web:ec67f4c19cfe7e490f475d',
-};
+const bytes = CryptoJS.AES.decrypt(
+  'U2FsdGVkX19C/p3xLukgTtrHxPGLuT2u2BfihWjPHeFUvpDCKM3y8A++YFmevyPKP2Ss6Wx07IQVZ5zoNQjBNvGwJQPZWcRU7hAGUh8udW1HyBSQ9vhbwHtpR6mxxRSBb7nPgfWeus4jML1AP3I5gCnFBrMci1Mz259zxZWHG/E4RIQbPh6JDwR7ECish6/UFelUp6Vwk+PvlG3KCaq+N2hj8dN7293bRYGyy+6Pv/kYFbjzBbDUJIo7ZYyCukGM7kkAIiErdmtE3/fdBxwp9stHqxfZTzpNxCmaOOsFV4v6YMBFzzbmqyaEmemqb7pSyiGLRO3hQTC5QabU4vu9GB/Zhv9MwvbqQh1qHbTOnAtrqUSmnCherVWaAcXjP2pxfS5rdTfaWKCYbMpOeZ02eet2bg0XuFaQum2r4waLgYtXtJfeqBNLpmm5cX7caDaMiUqMPeQcR/r0bO1cYZnqj/TjOzBvcqdN6BFYvEJ52+aXHGQAUQABoBlV5ZVFloYGH61XRGKtzXGQZdyiqvifH5VZL6nYxXw0V++a7kxE1EA=',
+  'OTP Sign',
+);
+const firebaseConfig = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
